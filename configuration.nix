e3d3feb24144c2a71a97ea688b5f05a1b35dce32
Togo-GT <michael.kaare.nielsen@gmail.com>
2025-09-16 14:11:01 +0200

    config back to 0

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,8 @@
   time.timeZone = "Europe/Copenhagen";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "da_DK.UTF-8";
+  i18n.defaultLocale = "en_DK.UTF-8";
+  i18n.supportedLocales = ["en_DK.UTF-8/UTF-8" "da_DK.UTF-8/UTF-8"];
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "da_DK.UTF-8";
@@ -46,6 +47,7 @@
   };
 
   # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
@@ -74,19 +76,27 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
-  # Enable touchpad support
-  services.xserver.libinput.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-  # Define a user account.
+  # Enable Zsh shell
+  programs.zsh.enable = true;
+
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.togo-gt = {
     isNormalUser = true;
     description = "Togo-GT";
-    extraGroups = [ "networkmanager" "wheel" "audio" "video" ];
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
     packages = with pkgs; [
       kdePackages.kate
-      firefox
+    #  thunderbird
     ];
   };
 
@@ -94,43 +104,76 @@
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "togo-gt";
 
+  # Install firefox.
+  programs.firefox.enable = true;
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile.
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    curl
-    git
-    htop
-    neofetch
-    # Nødvendige værktøjer
-    pciutils    # lspci
-    usbutils    # lsusb
-    # Lystilføjelser
-    libreoffice
-    gimp
-    vlc
-  ];
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Enable Flatpak support
-  services.flatpak.enable = true;
-
-  # Enable OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Enable Bluetooth
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  # This value determines the NixOS release
-  system.stateVersion = "25.05"; # Did you read the comment?
-
-  # Garbage collection for Nix store
+  # Automatic garbage collection
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
+    htop
+    file
+    unzip
+    p7zip
+    # KDE applications
+    kdePackages.dolphin
+    kdePackages.konsole
+    # Useful utilities
+    ripgrep
+    fd
+    eza
+  ];
+
+  # Enable TRIM for SSDs
+  services.fstrim.enable = true;
+
+  # Enable early OOM daemon to handle low memory situations
+  services.earlyoom.enable = true;
+
+  # Enable Bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
