3e996c3e7f57b31eda497043ae2b90cc0840505d
Togo-GT <michael.kaare.nielsen@gmail.com>
2025-09-20 00:14:53 +0200

    1

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     # Home Manager for user-specific configuration
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
